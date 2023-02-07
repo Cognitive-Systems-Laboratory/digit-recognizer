911d49e539f7e0c0e7e609dba52e5760b2ea5109
1pha <1phantasmas@korea.ac.kr>
2023-02-07 15:17:35 +0900

    Add instantiate

--- hydra_test/test.py
+++ hydra_test/test.py
@@ -4,6 +4,7 @@ from omegaconf import DictConfig, OmegaConf
 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def test(cfg: DictConfig) -> None:
+    loss_fn = hydra.utils.instantiate(cfg.loss)
     breakpoint()
     print(OmegaConf.to_yaml(cfg))
 
