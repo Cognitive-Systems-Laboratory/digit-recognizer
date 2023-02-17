72aa98e920c832abb89e9915b399fab1a9592a60
1pha <1phantasmas@korea.ac.kr>
2023-02-17 16:36:47 +0900

    Add week 4

--- digitrec/trainer.py
+++ digitrec/trainer.py
@@ -4,6 +4,9 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import DataLoader
+import torchmetrics.functional.classification as tfc
+from tqdm import tqdm
+import wandb
 
 
 class Trainer:
@@ -22,26 +25,43 @@ class Trainer:
         for mode, loader in loaders.items():
             setattr(self, f"{mode}_dataloader", loader)
 
-    def _epoch(self, dataloader: DataLoader, update_params: bool = True):
-        for img, y_true in dataloader:
+    def _epoch(self, dataloader: DataLoader, epoch: int, update_params: bool = True, mode: str = "train"):
+        progress_bar = tqdm(
+            dataloader,
+            desc=f'{mode.capitalize()} Epoch {epoch}',
+            leave=False,
+            total=len(dataloader),
+        )
+        total_loss = 0
+        _y_true, _y_pred = [], []
+        for idx, (img, y_true) in enumerate(progress_bar):
             img, y_true = img.to(self.device), y_true.to(self.device)
             y_pred = self.model(img)
 
             loss = self.loss_fn(y_pred, y_true)
+            total_loss += loss.item()
+
+            _y_true.append(y_true)
+            _y_pred.append(y_pred)
 
             if update_params:
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
+        _y_true = torch.cat(_y_true, dim=0).cpu().detach()
+        _y_pred = torch.cat(_y_pred, dim=0).cpu().detach()
+        wandb.log({f"{mode}/loss": total_loss / (idx + 1)}, step=epoch)
+        wandb.log({f"{mode}/acc": tfc.multiclass_accuracy(preds=_y_pred, target=_y_true, num_classes=10)}, step=epoch)
+        wandb.log({f"{mode}/auroc": tfc.multiclass_auroc(preds=_y_pred, target=_y_true, num_classes=10)}, step=epoch)
 
     def fit(self, epochs: int = None):
         epochs = epochs or self.epochs
         for e in range(epochs):
             self.model.train()
-            self._epoch(dataloader=self.train_dataloader, update_params=True)
+            self._epoch(dataloader=self.train_dataloader, epoch=e, update_params=True, mode="train")
 
             self.model.eval()
-            self._epoch(dataloader=self.valid_dataloader, update_params=False)
+            self._epoch(dataloader=self.valid_dataloader, epoch=e, update_params=False, mode="valid")
         self.save_checkpoint(model=self.model, model_dir="./ckpt.pt")
         
     def save_checkpoint(self, model: nn.Module, model_dir: str):
@@ -49,7 +69,6 @@ class Trainer:
 
 
 def setup_trainer(config) -> Trainer:
-
     model: nn.Module = timm.create_model(**config.model)
     optimizer = hydra.utils.instantiate(config.optimizer, params=model.parameters())
     device: str = config.device
--- wandb_example.py
+++ wandb_example.py
@@ -1,20 +1,17 @@
+import numpy as np
 import hydra
 import omegaconf
 import wandb
 
-from digitrec.trainer import setup_trainer
-
-
 @hydra.main(version_base="1.3", config_path="configs", config_name="train")
 def main(cfg: omegaconf.DictConfig) -> None:
     wandb.init(
-        project=cfg.logging.project,
-        name=cfg.logging.name,
+        project="test-project",
+        name="test-run",
         config=omegaconf.OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True),
     )
-    trainer = setup_trainer(config=cfg)
-    trainer.fit()
-
+    for idx, val in enumerate(np.random.random(size=20)):
+        wandb.log({"foo": val}, step=idx)
 
 if __name__=="__main__":
     main()
